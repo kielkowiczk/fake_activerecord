001005346d8c08def1e90cfe79a2a70497f6a327
Kazimierz Kiełkowicz <kielkowicz.kazimierz@gmail.com>
2012-05-05 17:36:57 +0200

    Moves adapters to separate file.

--- fake_ar.php
+++ fake_ar.php
@@ -1,15 +1,6 @@
 <?php
 include 'tools.php';
-
-abstract class FakeAbstrackAdapter {
-	abstract public function select($class_name, $name, $args);
-}
-
-class FakeAdapter extends FakeAbstrackAdapter {
-	public function select($class_name, $name, $args) {
-		puts("Trying to find/select in/from ".$class_name." table by field:".$name." with value:".$args -> first());
-	}
-}
+include 'adapters.php';
 
 class FakeActiveModel {
 	protected $properties;
