aecb23568ef17553fc8713dc1f3b603006067262
Kazimierz Kiełkowicz <kielkowicz.kazimierz@gmail.com>
2012-05-05 17:34:20 +0200

    create FakeAbstrackAdapter and FakeAdapter to abstract from database.

--- fake_ar.php
+++ fake_ar.php
@@ -1,6 +1,16 @@
 <?php
 include 'tools.php';
 
+abstract class FakeAbstrackAdapter {
+	abstract public function select($class_name, $name, $args);
+}
+
+class FakeAdapter extends FakeAbstrackAdapter {
+	public function select($class_name, $name, $args) {
+		puts("Trying to find/select in/from ".$class_name." table by field:".$name." with value:".$args -> first());
+	}
+}
+
 class FakeActiveModel {
 	protected $properties;
 	
@@ -18,11 +28,9 @@ class FakeActiveModel {
 
 	static public function __callStatic($name, $args) {
 		$class_name = get_called_class();
-		finder($class_name, str_replace('find_by_', '', $name), new SmartArray($args));
+		$adapter = new FakeAdapter();
+		$adapter -> select($class_name, str_replace('find_by_', '', $name), new SmartArray($args));
 	}	
 }
 
-function finder($class_name, $name, $args) {
-	print("Trying to find in ".$class_name." table by field:".$name." with value:".$args -> first()."\n");
-}
 ?>
