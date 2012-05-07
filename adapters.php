f6ba68cf3dea9a86bd59dd7507728eea9cb13cb9
Kazimierz Kiełkowicz <kielkowicz.kazimierz@gmail.com>
2012-05-07 16:58:06 +0200

    Add factory to select adapters based on configuration in config.php file.

--- adapters.php
+++ adapters.php
@@ -1,11 +1,66 @@
 <?php
 abstract class FakeAbstrackAdapter {
-	abstract public function select($class_name, $name, $args);
+	public function select($class_name, $name, $args)
+	{
+		$this -> connect();
+		puts("Trying to find/select in/from ".$class_name." table by field:".$name." with value:".$args -> first());
+	}
+
+	abstract public function connect();
 }
 
-class FakeAdapter extends FakeAbstrackAdapter {
-	public function select($class_name, $name, $args) {
-		puts("Trying to find/select in/from ".$class_name." table by field:".$name." with value:".$args -> first());
+class FakeAdapterAbstract extends FakeAbstrackAdapter {
+	public function connect() {
+		puts('Connecting to nothing...');
+	}
+//	public function select($class_name, $name, $args) {
+//		puts("Trying to find/select in/from ".$class_name." table by field:".$name." with value:".$args -> first());
+//	}
+}
+class FakeAdapterMySQL extends FakeAbstrackAdapter {
+	public function connect() {
+		puts('Connecting to MySQL...');
+	}
+
+//	public function select($class_name, $name, $args) {
+//		puts("Trying to find/select in/from ".$class_name." table by field:".$name." with value:".$args -> first());
+//	}
+}
+
+class FakeAdapterPosgrsql extends FakeAbstrackAdapter {
+	public function connect() {
+		puts('Connecting to Posgresql...');
+	}
+
+//	public function select($class_name, $name, $args) {
+//		puts("Trying to find/select in/from ".$class_name." table by field:".$name." with value:".$args -> first());
+//	}
+}
+
+class FakeAdapterFactory {
+	private $adapter;
+
+	static public function create($config) {
+		puts($config['adapter']);
+
+		switch($config['adapter']) {
+			case 'abstract':
+				$adapter = new FakeAdapterAbstract();
+			break;
+			
+			case 'mysql':
+				$adapter = new FakeAdapterMySQL();
+			break;
+	
+			case 'postresql':
+				$adapter = new FakeAdapterPosgrsql();
+			break;
+
+			default:
+				throw new Exception('Unknown adapter.');
+			break;
+		}
+		return $adapter; 	
 	}
 }
 ?>
--- fake_ar.php
+++ fake_ar.php
@@ -1,6 +1,7 @@
 <?php
 include 'tools.php';
 include 'adapters.php';
+include 'config.php';
 
 class FakeActiveModel {
 	protected $properties;
@@ -19,8 +20,9 @@ class FakeActiveModel {
 	}
 
 	static public function __callStatic($name, $args) {
+		global $config;
 		$class_name = get_called_class();
-		$adapter = new FakeAdapter();
+		$adapter = FakeAdapterFactory::create($config);
 		$adapter -> select($class_name, str_replace('find_by_', '', $name), new SmartArray($args));
 	}	
 }
